ab0da402389d262d757efb6b6685d49320573708
Andreykk07 <135097106+Andreykk07@users.noreply.github.com>
2023-11-29 14:25:14 +0100

    Create Singleton.cpp

---
+++ Singleton.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Singleton {
+public:
+    static Singleton& getInstance() {
+        static Singleton instance; // Єдиний екземпляр класу
+        return instance;
+    }
+
+    void someMethod() {
+        std::cout << "Виклик методу класу Singleton" << std::endl;
+    }
+
+private:
+    Singleton() {}
+    
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+};
+
+int main() {
+    Singleton& instance = Singleton::getInstance();
+    
+    instance.someMethod();
+
+    return 0;
+}
